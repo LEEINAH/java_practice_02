727b3a390083aa298be2a3c6556894e1b7314ba7
LEEINAH <22na7592@gmail.com>
2025-02-12 12:31:39 +0900

    컨트롤러 도입

--- src/main/java/com/koreait/App.java
+++ src/main/java/com/koreait/App.java
@@ -3,15 +3,31 @@ package com.koreait;
 import java.util.Scanner;
 
 public class App {
+    private Scanner sc;
+    private TodoController todoController;
+
+    public App() {
+        sc = new Scanner(System.in);
+        todoController = new TodoController();
+    }
+
     public void run() {
-        System.out.println("Todo App 시작");
-        
-        try(Scanner sc = new Scanner(System.in)) {
-            System.out.println("명령어) ");
+        System.out.println("TodoApp 시작");
+        while (true) {
+            System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
-            
-            System.out.println();
+            if (cmd.equals("exit")) break;
+            else if (cmd.equals("add")) {
+                todoController.add();
+            } else if (cmd.equals("list")) {
+                todoController.list();
+            } else if (cmd.equals("del")) {
+                todoController.del();
+            } else if (cmd.equals("modify")) {
+                todoController.modify();
+            }
         }
-        System.out.println("Todo App 끝");
+        System.out.println("TodoApp 끝");
+        sc.close();
     }
 }
